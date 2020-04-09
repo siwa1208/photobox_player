ac871365ec01061b9fdfd7158156124fce8dc203
siwa1208 <siwa.bp@gmail.com>
2020-04-10 01:55:44 +0200

    update style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="app">    
-            <b-navbar style="background: linear-gradient(200deg, #BD5DCD,#824AA7,#604591,#3F2D86,#362472); margin-bottom: 2%;">
+            <b-navbar style="background: linear-gradient(200deg, #BD5DCD,#824AA7,#604591,#3F2D86,#362472); margin-bottom: 2%; color: white">
                 <a class="navbar-brand" href="/">
-                        <img src="./assets/photobox_logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                        <img src="./assets/photobox_logo.png" width="30" height="30" class="d-inline-block align-top">
                         Photobox
                     </a>
-                <b-navbar-nav class="justify-content-end">
+                <b-navbar-nav class="justify-content-start">
                     <b-nav-item to="/">Home</b-nav-item>
                     <b-nav-item to="/Photos">Photos</b-nav-item>            
                 </b-navbar-nav>
